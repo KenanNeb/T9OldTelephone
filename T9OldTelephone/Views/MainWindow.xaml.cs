08e3f04056b5811621cc86cc01e5d1dd41631aae
NXbi_av4i@itstep.edu.az <NXbi_av4i@itstep.edu.az>
2022-11-18 02:42:29 +0400

    Add project files.

---
+++ T9OldTelephone/Views/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using T9OldTelephone.ViewModels;
+
+namespace T9OldTelephone
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new MainViewModel() { T9Keyboard = this };
+        }
+    }
+}
